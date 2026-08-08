c6c541b3be11907a05a34111f80e334d43576dcd
ZackHine <8602204+ZackHine@users.noreply.github.com>
2026-08-08 17:57:49 +0000

    agentpm: update formula to 0.1.21
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/agentpm.rb
+++ Formula/agentpm.rb
@@ -1,23 +1,23 @@
 class Agentpm < Formula
   desc "AgentPM CLI"
   homepage "https://github.com/agentpm-dev/cli"
-  version "0.1.20"
+  version "0.1.21"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/agentpm-dev/cli/releases/download/v0.1.20/agentpm-aarch64-apple-darwin.tar.gz"
-      sha256 "8012675de4426a34e9919f23271432c516dc637cc414370fdb2ccd09d5a6811c"
+      url "https://github.com/agentpm-dev/cli/releases/download/v0.1.21/agentpm-aarch64-apple-darwin.tar.gz"
+      sha256 "a48547bd4add178cbdd29981152b0d1dc43cce5c211da1aa2094d47ccdfd71b7"
     else
-      url "https://github.com/agentpm-dev/cli/releases/download/v0.1.20/agentpm-x86_64-apple-darwin.tar.gz"
-      sha256 "c456d355c3d53a45bfb1ea6ea41602311c949a78a81bba1354e472126dbaf9c6"
+      url "https://github.com/agentpm-dev/cli/releases/download/v0.1.21/agentpm-x86_64-apple-darwin.tar.gz"
+      sha256 "5fd8fa66a16b8d0fe8cd9e1be56a829bf71c168796c31507d0ed1b0c5b0940a4"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/agentpm-dev/cli/releases/download/v0.1.20/agentpm-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8950436dcd4264f5b9a110056c8e0414e43e589486ffe345d1d93061c38396ba"
+      url "https://github.com/agentpm-dev/cli/releases/download/v0.1.21/agentpm-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "e93c442328bbb098810da14ff75f307d2598706ae5fc81fecd183d083950e908"
     else
       odie "Unsupported architecture"
     end
